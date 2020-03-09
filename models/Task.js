0d615b93209fade3a6f4531502a0a49c3a3d11ad
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-09 11:39:25 +0600

    Сделал post запросы на (/users) и (/users/sessions)

---
+++ models/Task.js
@@ -0,0 +1,22 @@
+/*
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: Number,
+        required: true
+    },
+    description: {
+      type: String
+    },
+
+});
+
+const Album = mongoose.model('Task', TaskSchema);
+
+module.exports = Album;*/
